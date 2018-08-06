efbd356e068e96e40b6255c4bb3006e2be36278c
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-06 13:52:21 -0500

    ninja.html
    Added ninja.html with 4 imgs and added /ninja route to tmnt.py. No issues

--- tmnt.py
+++ tmnt.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/ninja')
+def ninja():
+    return render_template('ninja.html')
+    
 app.run(debug=True)
